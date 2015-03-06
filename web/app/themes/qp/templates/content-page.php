95c7d12070e8a3c901deb5aa46aff26d8ce5a77b
Fredrik Broman <frebro@gmail.com>
2015-03-06 10:13:19 +0100

    conditionally showing the_content on pages when not empty

--- web/app/themes/qp/templates/content-page.php
+++ web/app/themes/qp/templates/content-page.php
@@ -1,2 +1,6 @@
-<?php the_content(); ?>
-<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+<?php if ( get_the_content() ) : ?>
+<div class="page-content">
+  <?php the_content(); ?>
+  <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+</div>
+<?php endif; ?>
