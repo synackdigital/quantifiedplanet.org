017d8b051ac1a61f359a436d16c1f835fecea4f3
Fredrik Broman <frebro@gmail.com>
2015-03-06 10:02:42 +0100

    added theme colors to the acf color picker

--- web/app/themes/qp/lib/extras.php
+++ web/app/themes/qp/lib/extras.php
@@ -36,3 +36,27 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+/**
+ * Insert custom colors in the ACF color picker
+ */
+function acf_admin_head() {
+  ?>
+  <script>
+    (function($){
+      acf.add_action('ready append', function() {
+        acf.get_fields({ type : 'color_picker'}).each(function() {
+          $(this).iris({
+            palettes: ['#36bfae', '#2ca2dc', '#fabd0c', '#2e2e2e', '#6f6d6e', '#e4e4e4'],
+            change: function(event, ui) {
+              $(this).parents('.wp-picker-container').find('.wp-color-result').css('background-color', ui.color.toString());
+            }
+          });
+        });
+      });
+    })(jQuery);
+  </script>
+  <?php
+}
+
+add_action( 'acf/input/admin_head', __NAMESPACE__ . '\\acf_admin_head' );
