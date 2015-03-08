45796d2ab5db4fa1c8d5f8b0c67193825508c547
Fredrik Broman <frebro@gmail.com>
2015-03-08 15:12:43 +0100

    acf textblock layout

--- web/app/themes/qp/templates/acf-textblock.php
+++ web/app/themes/qp/templates/acf-textblock.php
@@ -1,20 +1,5 @@
-<?php
-  switch (get_sub_field('layout')) :
-    case 'left':
-    default:
-      $colClasses = 'col-xs-12 col-sm-10 col-md-8 col-lg-6';
-    break;
-    case 'center':
-      $colClasses = 'col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3';
-    break;
-    case 'right':
-      $colClasses = 'col-xs-12 col-sm-10 col-sm-offset-2 col-md-8 col-md-offset-4 col-lg-6 col-lg-offset-6';
-    break;
-  endswitch;
-?>
-
-<div class="acf-textblock row">
-  <div class="<?php echo $colClasses.' '.get_sub_field('size'); ?>" style="color:<?php the_sub_field('color'); ?>">
+<div class="acf acf-textblock">
+  <div class="layout layout-<?php the_sub_field('layout'); ?> <?php the_sub_field('size'); ?>" style="color:<?php the_sub_field('color'); ?>;">
     <?php the_sub_field('text'); ?>
   </div>
 </div>
