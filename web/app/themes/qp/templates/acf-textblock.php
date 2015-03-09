c456349c71bf4504bbb26da762b91acd9b83f170
Fredrik Broman <frebro@gmail.com>
2015-03-09 10:42:37 +0100

    enabled titles for all acf section types + smoother typography for acf-textblock

--- web/app/themes/qp/templates/acf-textblock.php
+++ web/app/themes/qp/templates/acf-textblock.php
@@ -1,5 +1,22 @@
-<div class="acf acf-textblock">
-  <div class="layout layout-<?php the_sub_field('layout'); ?> <?php the_sub_field('size'); ?>" style="color:<?php the_sub_field('color'); ?>;">
-    <?php the_sub_field('text'); ?>
+<?php
+
+$title = get_sub_field('title');
+$content = get_sub_field('content');
+
+?>
+
+<section class="acf acf-textblock" style="color:<?php the_sub_field('color'); ?>;">
+  <div class="layout layout-<?php the_sub_field('layout'); ?>">
+
+    <?php if ($title) : ?>
+      <h1><?php echo $title; ?></h1>
+    <?php endif; ?>
+
+    <?php if ($content) : ?>
+      <div class="content <?php the_sub_field('size'); ?>">
+        <?php echo $content; ?>
+      </div>
+    <?php endif; ?>
+
   </div>
-</div>
+</section>
--- web/app/themes/qp/templates/acf-wysiwyg.php
+++ web/app/themes/qp/templates/acf-wysiwyg.php
@@ -1,5 +1,22 @@
-<div class="acf acf-wysiwyg">
+<?php
+
+$title = get_sub_field('title');
+$content = get_sub_field('content');
+
+?>
+
+<section class="acf acf-wysiwyg">
   <div class="layout layout-<?php the_sub_field('layout'); ?>">
-    <?php the_sub_field('mce'); ?>
+
+    <?php if ($title) : ?>
+      <h1><?php echo $title; ?></h1>
+    <?php endif; ?>
+
+    <?php if ($content) : ?>
+      <div class="content">
+        <?php echo $content; ?>
+      </div>
+    <?php endif; ?>
+
   </div>
-</div>
+</section>
