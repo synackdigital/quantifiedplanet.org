c670715ef7d1f28f51780a8f136f1229f4faa938
Fredrik Broman <frebro@gmail.com>
2015-03-10 21:50:20 +0100

    removed acf section titles

--- web/app/themes/qp/templates/acf-wysiwyg.php
+++ web/app/themes/qp/templates/acf-wysiwyg.php
@@ -1,16 +1,11 @@
 <?php
 
-$title = get_sub_field('title');
 $content = get_sub_field('content');
 
 ?>
 
 <section class="acf acf-wysiwyg">
   <div class="layout layout-<?php the_sub_field('layout'); ?>">
-
-    <?php if (1===0 && $title) : ?>
-      <h1 class="title"><?php echo $title; ?></h1>
-    <?php endif; ?>
 
     <?php if ($content) : ?>
       <div class="content">
