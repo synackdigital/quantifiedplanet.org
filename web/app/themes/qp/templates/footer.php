e572dea863b00b38e09bb8c4b1112dcef7898301
Fredrik Broman <frebro@gmail.com>
2015-03-11 13:38:45 +0100

    added footer content and layout diff --git a/web/app/themes/qp/assets/images/qpsymbol.svg b/web/app/themes/qp/assets/images/qpsymbol.svg new file mode 100644 index 0000000..a3cd013 --- /dev/null +++ b/web/app/themes/qp/assets/images/qpsymbol.svg @@ -0,0 +1,12 @@ +<?xml version="1.0" encoding="utf-8"?> +<!-- Generator: Adobe Illustrator 18.1.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  --> +<svg version="1.1" id="qpsymbol" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" +	 viewBox="-37.7 492.2 75.4 135.5" enable-background="new -37.7 492.2 75.4 135.5" xml:space="preserve"> +<g id="symbol"> +	<path stroke="#000000" stroke-width="0.1" stroke-miterlimit="10" d="M-3.5,593.9c-18.9,0-34.1-15-34.1-33.9 +		c0-18.9,15.3-33.9,33.9-33.9s34.1,15.3,34.1,33.9C30.4,578.6,15.1,593.9-3.5,593.9z M20,560c0-13.2-10.6-23.8-23.8-23.8 +		s-23.8,10.6-23.8,23.8c0,13.2,10.6,23.8,23.8,23.8S20,573.2,20,560z"/> +	<rect x="-8.7" y="492.2" width="10.3" height="135.5"/> +	<rect x="10" y="549.2" transform="matrix(-0.707 0.7072 -0.7072 -0.707 433.1462 972.1718)" width="10.3" height="53.3"/> +</g> +</svg> diff --git a/web/app/themes/qp/assets/styles/layouts/_footer.less b/web/app/themes/qp/assets/styles/layouts/_footer.less index ea2918d..5884180 100755 --- a/web/app/themes/qp/assets/styles/layouts/_footer.less +++ b/web/app/themes/qp/assets/styles/layouts/_footer.less @@ -1,4 +1,26 @@  .content-info {    background-color: @gray-darker; -  color: white; +  color: @gray-light; + +  padding-top: (@line-height-computed * 2); +  padding-bottom: (@line-height-computed * 2); +} + +.footer-brand { + +  display: inline-block; +  float: left; +  margin-right: @padding-large-horizontal; +  width: 60px; + +  svg { +    width: 100%; + +    #symbol { +      * { +        fill: @gray-light; +        stroke: @gray-light; +      } +    } +  }  } diff --git a/web/app/themes/qp/templates/footer.php b/web/app/themes/qp/templates/footer.php index 3c83574..c3131f9 100755 --- a/web/app/themes/qp/templates/footer.php +++ b/web/app/themes/qp/templates/footer.php @@ -1,5 +1,11 @@  <footer class="content-info" role="contentinfo">    <div class="container"> -    <?php dynamic_sidebar('sidebar-footer'); ?> + +    <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-0"> +      <div class="footer-brand"><?php include(get_stylesheet_directory().'/assets/images/qpsymbol.svg'); ?></div> +      <p class="strapline"><?php the_field('strapline', 'option'); ?></p> +      <p class="copyright small"><?php the_field('copyright', 'option'); ?></p> +    </div> +    </div>  </footer>

--- web/app/themes/qp/templates/footer.php
+++ web/app/themes/qp/templates/footer.php
@@ -1,5 +1,11 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+
+    <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-0">
+      <div class="footer-brand"><?php include(get_stylesheet_directory().'/assets/images/qpsymbol.svg'); ?></div>
+      <p class="strapline"><?php the_field('strapline', 'option'); ?></p>
+      <p class="copyright small"><?php the_field('copyright', 'option'); ?></p>
+    </div>
+
   </div>
 </footer>
